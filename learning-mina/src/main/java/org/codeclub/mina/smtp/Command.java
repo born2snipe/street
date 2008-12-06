cb45743d7d088d52bdc1459d8586742c80835da3
Dan Dudley <born2snipe@gmail.com>
2008-12-06 10:12:20 -0600

    how does merging work...

--- learning-mina/src/main/java/org/codeclub/mina/smtp/Command.java
+++ learning-mina/src/main/java/org/codeclub/mina/smtp/Command.java
@@ -6,7 +6,7 @@ import org.apache.mina.core.session.IoSession;
  * Created by IntelliJ IDEA.
  * User: dan
  * Date: Nov 23, 2008
- * Time: 10:34:16 AM
+ * Time: 10:34:60000 AM
  * To change this template use File | Settings | File Templates.
  */
 public interface Command {
